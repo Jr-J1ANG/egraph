459e74aa4c35c9e4b5558448a2f78783306484d1
Jerry <jrjiang@g.ecc.u-tokyo.ac.jp>
2026-09-23 23:32:28 +0900

    Remove duplicate Id::from(2) from local_scope

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // These will later be generated from the raw EGraph.
     let local_scope = vec![
         Id::from(0),
-        Id::from(2),
+        Id::from(1),
         Id::from(2),
         Id::from(3),
         Id::from(4),
@@ -66,7 +66,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let local_scope = vec![
         Id::from(0),
-        Id::from(2),
+        Id::from(1),
         Id::from(2),
         Id::from(3),
         Id::from(4),
